f8acecfda6b07d3078911b79ebf49262aa07a839
lindaruedaflores <lindaruedaflores@gmail.com>
2018-07-18 14:02:42 -0700

    Revert "Revert "Merge branch 'master' of https://github.com/AlbertoR17/pv""
    This reverts commit 83794e7826cca8793d491b26cfc42268138d79b9.

--- Vista/busca_producto.php
+++ Vista/busca_producto.php
@@ -3,11 +3,12 @@ require('conec.php');
  
 $codigo=$_POST['codigo'];
 $rs = mysqli_query($con,"SELECT * FROM productos where Codigo=$codigo");
-  
+    
                        $array=array();
                        
                        //$i=0;
-                       while($row = mysqli_fetch_assoc($rs)){
+                       while($row =mysqli_fetch_assoc($rs)){
+                          $id =$row['Id_Producto'];
                           $codigoPro =$row['Codigo'];
                           $nom=$row['Nombre'];
                           $PrecioU=$row['Precio_Unitario'];
@@ -18,11 +19,14 @@ $rs = mysqli_query($con,"SELECT * FROM productos where Codigo=$codigo");
                           //$i++;
                         //$array[] = array( 'Nombre'=> $nom,'Precio_Unitario'=> $PrecioU,'Descripcion'=> $Des);
                       }
-                      //$array[] = array( 'Nombre'=> $nom,'Precio_Unitario'=> $PrecioU,'Descripcion'=> $Des);
-                       
-                      mysqli_close($con);
+                       mysqli_close($con);
+                    
+                    echo json_encode( array('Nombre'=> $nom,'Precio_Unitario'=> $PrecioU,'Descripcion'=> $Des,'Imagen'=> $im,'Id_Producto'=> $id));
                    
-                    echo json_encode( array('Nombre'=> $nom,'Precio_Unitario'=> $PrecioU,'Descripcion'=> $Des,'Imagen'=> $im));
+                  
+                   
+                      
+                     
                     
 ?>  
 
